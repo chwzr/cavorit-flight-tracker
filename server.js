8a9959fc4ff1948a45c9fa76304a5d9873223503
chwzr <felix@koppe.design>
2019-03-05 01:05:09 +0100

    fix server ip

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, '192.168.2.23', err => {
+    server.listen(3000, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
